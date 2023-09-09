7651545ead6c8bd7d40f85b08d2a8feeeb6fd4d9
Polya008 <polskaya007@mail.ru>
2023-09-09 17:03:45 +0300

    correct jest

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   target: 'web',
+  testEnvironment: 'jsdom',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
